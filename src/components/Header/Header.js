d77fbcdf2b8eab5c8133dd99d47313b2130fa393
agboola.omowumi <agboola.omowumi@konga.com>
2020-06-07 22:15:56 +0100

    Add responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,34 +5,46 @@ import Button from "../Button/Button";
 
 import Logo from "../Logo/logo";
 
+import usBanner from "../../assests/images/us-banner.png";
+import icon from "../../assests/images/icon.png";
+
 class Header extends Component {
   render() {
     return (
       <Fragment>
         <header className={classes.hero}>
+        <div className={classes.top}>
           <div className={classes.navbar}>
-            <Logo />
-            <nav>
-              <ul>
-                <li>
-                  <a class="current" href="index.html">
-                    OVERVIEW
-                  </a>
-                </li>
-                <li>
-                  <a href="about.html">FEATURES </a>
-                </li>
-                <li>
-                  <a href="contact.html"> TECHNOLOGY </a>
-                </li>
-                <li>
-                  <a href="contact.html"> CONTACT</a>
-                </li>
-                <li>
-                  <a href="contact.html">SIGNUP</a>
-                </li>
-              </ul>
-            </nav>
+            
+              <Logo />
+              <nav>
+                <ul>
+                  <li>
+                    <a class="current" href="index.html">
+                      OVERVIEW
+                    </a>
+                  </li>
+                  <li>
+                    <a href="about.html">FEATURES </a>
+                  </li>
+                  <li>
+                    <a href="contact.html"> TECHNOLOGY </a>
+                  </li>
+                  <li>
+                    <a href="contact.html"> CONTACT</a>
+                  </li>
+                  <li>
+                    <a href="contact.html">SIGNUP</a>
+                  </li>
+                </ul>
+              </nav>
+              <div className={classes.login}>
+                <img src={usBanner} alt={"us-banner"} />
+                <p>EN</p>
+                <img src={icon} alt={"icon"} className={classes.icon} />
+                <button className={classes.button}>Login</button>
+              </div>
+            </div>
           </div>
           <div className={classes.content}>
             <h1>CloudBudget</h1>
